ecb75726cfca0a7dbc541d7a022a78c95b41cb3d
Thangadurai2830 <thangaduraibeit100@gmail.com>
2024-09-13 19:16:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+<a name="readme-top"></a>
+
+# Exercise_API
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<!-- [![Product Name Screen Shot][product-screenshot]](https://example.com) -->
+
+This API gives you access to over 1300 exercises with individual exercise data and animated demonstrations
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+### ListAllExercises
+
+Returns All the exercises
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+### ListByName
+
+Returns the exercises that similar the name field
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises/searchbyname/{name}')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+
+### ListofBodyParts
+
+Returns the list of BodyParts
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises/bodypartlist')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+
+### ListByBodyPart
+
+Returns the exercises that trains the BodyPart
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises/listbybodypart/{bodypart}')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+
+### ListById
+
+Return the exercise that with the searched id
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises/searchbyid/{id}')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+
+### ListOftargetMuscles
+
+Returns the list of Target Muscle
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises/listoftargetmuscles')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+
+### ListByTargetMuscle
+
+Returns the exercises that trains Target Muscle
+
+```js
+fetch('https://exercise-api-lyart.vercel.app/exercises/listbytargetmuscle/:muscle')
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+```
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ index.js
@@ -0,0 +1,153 @@
+require("dotenv").config();
+const express = require("express");
+const bodParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.set("viewengine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+});
+
+const exerciseSchema = new mongoose.Schema({
+  bodyPart: String,
+  equipment: String,
+  gifUrl: String,
+  id: String,
+  name: String,
+  target: String,
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+app.get("/", function (req, res) {
+  var someHTML =
+    '<a href="https://github.com/avvicky/Exercise_API">Github Readme</a>';
+  res.send(someHTML);
+});
+
+app.get("/exercises", function (req, res) {
+  Exercise.find(function (err, foundExercises) {
+    if (!err) {
+      res.send(foundExercises);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.get("/exercises/bodypartlist", function (req, res) {
+  res.send([
+    "back",
+    "cardio",
+    "chest",
+    "lower arms",
+    "lower legs",
+    "neck",
+    "shoulders",
+    "upper arms",
+    "upper legs",
+    "waist",
+  ]);
+});
+
+app.get("/exercises/listbybodypart/:bodypart", function (req, res) {
+  var requestedbodypart = req.params.bodypart;
+  if (requestedbodypart == "all") {
+    requestedbodypart = "";
+  }
+  Exercise.find(
+    { bodyPart: { $regex: ".*" + requestedbodypart + ".*" } },
+    function (err, foundExercises) {
+      if (!err) {
+        res.send(foundExercises);
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
+app.get("/exercises/searchbyname/:name", function (req, res) {
+  const requestedexercisename = req.params.name;
+
+  Exercise.find(
+    { name: { $regex: ".*" + requestedexercisename + ".*" } },
+    function (err, foundExercises) {
+      if (!err) {
+        res.send(foundExercises);
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
+app.get("/exercises/searchbyid/:id", function (req, res) {
+  const requestedExerciseId = req.params.id;
+  Exercise.find({ id: requestedExerciseId }, function (err, foundExercises) {
+    if (!err) {
+      res.send(foundExercises);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.get("/exercises/listoftargetmuscles", function (req, res) {
+  res.send([
+    "abductors",
+    "abs",
+    "adductors",
+    "biceps",
+    "calves",
+    "cardiovascular system",
+    "delts",
+    "forearms",
+    "glutes",
+    "hamstrings",
+    "lats",
+    "levator scapulae",
+    "pectorals",
+    "quads",
+    "serratus anterior",
+    "spine",
+    "traps",
+    "triceps",
+    "upper back",
+  ]);
+});
+
+app.get("/exercises/listbytargetmuscle/:muscle", function (req, res) {
+  const targetedExerciseMuscle = req.params.muscle;
+  Exercise.find(
+    { target: { $regex: ".*" + targetedExerciseMuscle + ".*" } },
+    function (err, foundExercises) {
+      if (!err) {
+        res.send(foundExercises);
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
+var port = process.env.PORT;
+
+if (port == null) {
+  port = 5000;
+}
+
+app.listen(port, function () {
+  console.log("server started on port ",port);
+});
